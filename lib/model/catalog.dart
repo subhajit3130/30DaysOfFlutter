7ae86b47409fb9e3934e3cd182ad27a6120bc874
subhajit3130 <goswami2.subhajit@gmail.com>
2021-10-22 09:30:01 +0530

    day 11 completed

--- lib/model/catalog.dart
+++ lib/model/catalog.dart
@@ -1,29 +1,88 @@
+import 'dart:convert';
+
 class Item {
-  final String id;
+  final int id;
   final String name;
   final String desc;
   final num price;
   final String color;
   final String image;
 
-  Item(
-      {required this.id,
-      required this.name,
-      required this.desc,
-      required this.price,
-      required this.color,
-      required this.image});
+  Item(this.id, this.name, this.desc, this.price, this.color, this.image);
+
+  Item copyWith({
+    int? id,
+    String? name,
+    String? desc,
+    num? price,
+    String? color,
+    String? image,
+  }) {
+    return Item(
+      id ?? this.id,
+      name ?? this.name,
+      desc ?? this.desc,
+      price ?? this.price,
+      color ?? this.color,
+      image ?? this.image,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'desc': desc,
+      'price': price,
+      'color': color,
+      'image': image,
+    };
+  }
+
+  factory Item.fromMap(Map<String, dynamic> map) {
+    return Item(
+      map['id'],
+      map['name'],
+      map['desc'],
+      map['price'],
+      map['color'],
+      map['image'],
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Item.fromJson(String source) => Item.fromMap(json.decode(source));
+
+  @override
+  String toString() {
+    return 'Item(id: $id, name: $name, desc: $desc, price: $price, color: $color, image: $image)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is Item &&
+        other.id == id &&
+        other.name == name &&
+        other.desc == desc &&
+        other.price == price &&
+        other.color == color &&
+        other.image == image;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^
+        name.hashCode ^
+        desc.hashCode ^
+        price.hashCode ^
+        color.hashCode ^
+        image.hashCode;
+  }
 }
 
 class CatalogItems {
-  static final items = [
-    Item(
-        id: "item_1",
-        name: "iPhone 13 Pro Max",
-        desc: "Apple iPhone 13th series",
-        price: 129900.00,
-        color: "#fdfdfa",
-        image:
-            "https://phonetechx.com/wp-content/uploads/2021/03/Apple-iPhone-13-Pro-Max.jpg")
-  ];
+  static List<Item> items = [];
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,14 +2,39 @@ import 'package:catalog_app/model/catalog.dart';
 import 'package:catalog_app/widgets/drawer.dart';
 import 'package:catalog_app/widgets/item_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    // await Future.delayed(Duration(seconds: 5));
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    // print(catalogJson.toString());
+    final decodedData = jsonDecode(catalogJson);
+    // print(decodedData);
+    var productData = decodedData["products"];
+    // print(productData.toString());
+    CatalogItems.items =
+        List.from(productData).map<Item>((e) => Item.fromMap(e)).toList();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    // int days = 30;
-    // String name = "codepur";
-
-    final dummyList = List.generate(50, (index) => CatalogItems.items[0]);
+    // final dummyList = List.generate(50, (index) => CatalogItems.items[0]);
 
     return Scaffold(
       appBar: AppBar(
@@ -18,10 +43,10 @@ class HomePage extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(10.0),
         child: ListView.builder(
-            itemCount: dummyList.length,
+            itemCount: CatalogItems.items.length,
             itemBuilder: (context, index) {
               return ItemWidget(
-                item: dummyList[index],
+                item: CatalogItems.items[index],
               );
             }),
       ),
